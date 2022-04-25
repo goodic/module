23f9ae03bc5845fef080f5d9796682ca4990ae7b
Sergey Gudik <sergey.goodic@gmail.com>
2022-04-25 21:59:15 +0300

    add import datetime

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from application import salary
 from application.db import people
+from datetime import datetime as dt
 
 
 if __name__ == '__main__':
+    print(dt.now())
     salary.calculate_salary()
     people.get_employees()
+
 
