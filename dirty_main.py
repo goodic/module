ca939ec6eb7e00d6b6e4e87949942d7a55783e03
Sergey Gudik <sergey.goodic@gmail.com>
2022-04-25 22:19:04 +0300

    add dirty main

---
+++ dirty_main.py
@@ -0,0 +1,9 @@
+from application.salary import *
+from application.db.people import *
+from datetime import *
+
+
+if __name__ == '__main__':
+    datetime.now()
+    calculate_salary()
+    get_employees()
